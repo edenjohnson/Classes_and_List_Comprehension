58b0fb911dd8544d9613ea4a360c072f3d35abb2
Eden Johnson <50639814+edenjohnson@users.noreply.github.com>
2019-10-18 13:01:04 -0700

    Update hw3_2.py

--- hw3_2.py
+++ hw3_2.py
@@ -1,18 +1,22 @@
 ###############################################################################
 # hw3_2.py
 # Author: Eden Johnson
-# Last Modified: 10/10/19
+# Last Modified: 10/18/19
 # Purpose: Write a object-oriented program that contains two classes, Deck and
 #          Card. To create a deck of card object, you need to write a class 
 #          called "Deck" using requirements, and it should use another class
 #          called "Card". 
 # Program Uses: Built-in Python functions len(), print(), range(). built-in
-#               Python methods .append(), .format(), .remove(). Use of class
+#               Python methods .append(), .format(), .pop(). Use of class
 #               creation with attributes and methods, import of the random
 #               module and use of its shuffle() method, list comprehension.
 ###############################################################################
 
 import random
+
+# assign the values and suits for the deck of cards
+value = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+suit =  ['Hearts','Diamonds','Clubs','Spades']
 
 class Card:
     def __init__(self, value, suit):
@@ -21,41 +25,32 @@ class Card:
         self.suit = suit        # hearts, diamonds, clubs, spades
 
 class Deck:
-    def __init__(self, list_of_cards):
+    def __init__(self):
         """ Constructor to initialize class Deck attributes that overrides
             Card class __init__ method. """
-        self.list_of_cards = list_of_cards
+        self.deck_of_cards = [Card(value[j], suit[i])\
+                              for i in range(len(suit))\
+                              for j in range(len(value))]
         
     def shuffle(self):
         """ Shuffle method should shuffle the full 52 card deck randomly. """
-        if len(self.list_of_cards) == 52:
-            random.shuffle(self.list_of_cards)
+        if len(self.deck_of_cards) == 52:
+            random.shuffle(self.deck_of_cards)
+            print("Shuffling the full deck of cards..\n")
         else:
             print("The size of your deck is {}"\
-                  .format(len(self.list_of_cards)) + \
+                  .format(len(self.deck_of_cards)) + \
                   " and is not big enough to shuffle.\n")
         
     def deal(self):
         """ Method should deal a single card from the deck and remove
             the card from the deck after it is dealt. """
-        card_deal = 0
-        for card in self.list_of_cards:
-            if card_deal == 0:
-                card_deal += 1
-                print("You have been dealt the {} ".format(card.value) \
-                       + "of {}.".format(card.suit) + "\n")
-                # delete the card from the deck after it has been dealt
-                self.list_of_cards.remove(card)   
-            
-value = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
-suit =  ['Hearts','Diamonds','Clubs','Spades']
+        dealt_card = self.deck_of_cards.pop()
+        print("You have been dealt the {} ".format(dealt_card.value) \
+                       + "of {}.".format(dealt_card.suit) + "\n")
 
-# create list of all 52 Card objects using list comprehension
-deck_list = [Card(value[j], suit[i]) for i in range(len(suit))\
-            for j in range(len(value))]
-
-# create the Deck
-deck = Deck(deck_list)
+# create the deck
+deck = Deck()
 
 # shuffle the deck, deal cards
 deck.shuffle()
